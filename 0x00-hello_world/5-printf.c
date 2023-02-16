bf1e816e670f7e62cda3321c94f9428b128bcb72
kmishmael <kmishmael@gmail.com>
2023-02-17 02:30:06 +0300

    Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - entrypoint
+ * Description: the entrypoint of our application
+ * Return: void
+ */
+int main(void)
+{
+	char string[] = "with proper grammar, but the outcome is a piece of art,";
+
+	printf("%s", string);
+
+}
