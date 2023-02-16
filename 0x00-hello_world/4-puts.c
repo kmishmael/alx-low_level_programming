6a79c1603c92c743147594a2c298e6d42e43b94d
kmishmael <kmishmael@gmail.com>
2023-02-17 02:12:45 +0300

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+/*
+ * The main function
+ */
+int main(void)
+{
+
+	char string[] = "Programming is like building a multilingual puzzle";
+
+	puts(string);
+
+	return (0);
+
+}
