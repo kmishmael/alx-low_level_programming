179de7dc5e65d4dbf0b190657e9cf30c50bfe2f6
kmishmael <kmishmael@gmail.com>
2023-02-17 02:15:53 +0300

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,12 @@
 #include<stdio.h>
 
-/*
- * The main function
+/**
+ * main - entry point of our application
  */
 int main(void)
 {
 
-	char string[] = "Programming is like building a multilingual puzzle";
+	char string[] = "\"Programming is like building a multilingual puzzle";
 
 	puts(string);
 
