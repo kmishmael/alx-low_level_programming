4a0a48796f53e621e1ed48fd9b312ae9cb3c8848
kmishmael <kmishmael@gmail.com>
2023-02-27 18:59:03 +0300

    half the lies

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -9,11 +9,13 @@
 void puts2(char *str)
 {
 	int i = 0;
+	int j = 0;
 
-	while (*str != '\0')
-	{
-		_putchar(*(str + i));
-		i = i + 2;
-	}
+	while (str[i++])
+		j++;
+
+	for (i = 0; i < j; i += 2)
+		_putchar(str[i]);
+
 	_putchar('\n');
 }
