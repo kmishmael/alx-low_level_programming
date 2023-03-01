efdbdf0cb5d2421a7065cf998753cd6e6d492f72
kmishmael <kmishmael@gmail.com>
2023-03-02 01:11:52 +0300

    strcpy

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -7,16 +7,16 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	int i = 0;
-	int len = strlen(s1);
+	int i = 0, ret;
 
-	while (i < len)
+	for (i = 0; s1[i] && s2[i]; i++)
 	{
-		if (s1[i] > s2[i])
-			return (15);
-		if (s1[i] < s2[i])
-			return (-15);
-		i++;
+		if (s1[i] == s2[i])
+			continue;
+		else
+			break;
 	}
-	return (0);
+
+	ret = s1[i] - s2[i];
+	return (ret);
 }
