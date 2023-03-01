dd58943924ad8269d3ebd0e492f0c4db69ce3dd5
kmishmael <kmishmael@gmail.com>
2023-03-02 00:40:01 +0300

    strncpy

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,22 @@
+
+/**
+ * _strncpy - copies string
+ * @dest: destination pointer
+ * @src: source pointer
+ * @n: size
+ * Return: char pointer
+ */
+char *_strncpy(char *dest, const char *src, int n)
+{
+	int i;
+
+	for (i = 0; i < n && src[i] != '\0'; i++)
+	{
+		dest[i] = src[i];
+	}
+	for ( ; i < n; i++)
+	{
+		dest[i] = '\0';
+	}
+	return (dest);
+}
