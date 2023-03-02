0f19da1ba2a1a52f227ed9a2d228fda0fc6d3332
kmishmael <kmishmael@gmail.com>
2023-03-02 20:45:39 +0300

    fix putchar

---
+++ 0x06-pointers_arrays_strings/_putchar.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * _putchar - output char
+ * @s: char
+ * Return: void
+ */
+void _putchar(char s)
+{
+	putchar(s);
+}
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,5 +1,5 @@
 #include <stddef.h>
-void _putchar(char *);
+void _putchar(char);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
