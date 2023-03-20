b8bd3639ed5f2d94a0d6fe5f8fb469bf46937475
kmishmael <kmishmael@gmail.com>
2023-03-20 22:03:44 +0300

    print struct

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,15 @@
+#include "dog.h"
+
+/**
+ * print_dog - print properties in dog struct
+ * @d: struct of type dog
+ */
+void print_dog(struct dog *d)
+{
+	if (d != NULL)
+	{
+		printf("Name: %s\n", d->name);
+		printf("Age: %f\n", d->age);
+		printf("Owner: %s\n", d->owner);
+	}
+}
