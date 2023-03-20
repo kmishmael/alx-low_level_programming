e056e50ad9f94dc915c9ffc40afaaebbb0b488fa
kmishmael <kmishmael@gmail.com>
2023-03-20 18:04:52 +0300

    poppy

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,12 @@
+/**
+ * struct dog - dog properties
+ * @name: dog name
+ * @age: the age of the dog
+ * @owner: the owner of the dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
