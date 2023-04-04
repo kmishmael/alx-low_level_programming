1e68e2d8f5257f72071a343325a232f1fe1785e6
kmishmael <kmishmael@gmail.com>
2023-04-04 17:10:52 +0300

    safe print

---
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+
+/**
+ * print_listint_safe - print a listint_t linked list
+ * @head: pointer to listint_t
+ * Return: size_t
+ */
+size_t print_listint_safe(const listint_t *head)
+{
+	size_t i = 0;
+	int len;
+
+	while (head != NULL)
+	{
+		len = head - head->next;
+		printf("[%p] %d\n", (void *)head, head->n);
+		i++;
+		if (len > 0)
+		{
+			head = head->next;
+		} else
+		{
+			printf("-> [%p] %d\n", (void *)head, head->next->n);
+			break;
+		}
+	}
+	return (i);
+}
