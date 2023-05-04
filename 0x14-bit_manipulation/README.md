f0b2b2363530ee4af81f831315458413e93d7229
kmishmael <kmishmael@gmail.com>
2023-05-04 23:41:58 +0300

    readd bit manipulation

--- 0x14-bit_manipulation/README.md
+++ 0x14-bit_manipulation/README.md
@@ -1,1 +1,2 @@
-0X014 - C - Bit manipulation
+# 0X014 - C - Bit manipulation
+Project on bit manipulation
--- 0x14-bit_manipulation/_putchar.c
+++ 0x14-bit_manipulation/_putchar.c
@@ -1,5 +1,10 @@
 #include "main.h"
 
+/**
+ * _putchar - custom putchar fn
+ * @s: char to write to out stream
+ * Return: void
+ */
 void _putchar(char s)
 {
 	putchar(s);
