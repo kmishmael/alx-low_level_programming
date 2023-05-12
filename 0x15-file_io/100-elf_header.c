8d87d4e4513ce94993fd7df313a6041279f399be
kmishmael <kmishmael@gmail.com>
2023-05-12 20:44:00 +0300

    read elf

---
+++ 0x15-file_io/100-elf_header.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * main - entrypoint function
+ * @argc: number of args
+ * @argv: arrays of args
+ * Return: int
+ */
+int main(int argc, char *argv)
+{
+
+	return (0);
+}
