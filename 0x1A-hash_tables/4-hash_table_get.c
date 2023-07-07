cfd2d8ac1dace1f415ee7085136dec444087ffb7
kmishmael <kmishmael@gmail.com>
2023-07-07 23:37:18 +0300

    handle nullity

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -11,6 +11,8 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	unsigned long int index;
 	hash_node_t *node;
 
+	if (ht == NULL || key == NULL || ht->size == 0)
+		return (NULL);
 	index = key_index((const unsigned char *)key, ht->size);
 	node = ht->array[index];
 	if (node == NULL)
