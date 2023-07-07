d2dd837e02e261ea92d904766386aa2ad0881d57
kmishmael <kmishmael@gmail.com>
2023-07-07 18:17:44 +0300

    add missing guard

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -18,7 +18,6 @@ hash_table_t *hash_table_create(unsigned long int size)
 	new_table->size = size;
 	new_table->array = malloc(sizeof(hash_node_t *) * size);
 	if (new_table->array == NULL)
-		free(new_table);
 		return (NULL);
 	i = 0;
 	while (i < size)
