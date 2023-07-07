c8b3a869735fc40d605c571b1d52767345a4c33e
kmishmael <kmishmael@gmail.com>
2023-07-07 23:12:12 +0300

    fix some edge errors

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,6 +1,35 @@
 #include "hash_tables.h"
 
 /**
+ * create_node - creates a new hash_node_t
+ * @key: the node key
+ * @value: the node value
+ * Return: new hash_node_t, else NULL
+ */
+hash_node_t *create_node(const char *key, const char *value)
+{
+	hash_node_t *new_node;
+
+	new_node = malloc(sizeof(hash_node_t));
+	if (new_node == NULL)
+		return (NULL);
+	new_node->key = strdup(key);
+	if (new_node->key == NULL)
+	{
+		free(new_node);
+		return (NULL);
+	}
+	new_node->value = strdup(value);
+	if (new_node->value == NULL)
+	{
+		free(new_node->key);
+		free(new_node);
+		return (NULL);
+	}
+	new_node->next = NULL;
+	return (new_node);
+}
+/**
  * hash_table_set - adds an element to the hash table
  * @ht: the hash table
  * @key: the key for insertion
@@ -19,24 +48,31 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		return (0);
 	}
 	index = key_index((const unsigned char *)key, ht->size);
-	new_node = malloc(sizeof(hash_node_t));
-	if (new_node == NULL)
-		return (0);
-	new_node->key = strdup(key);
-	if (new_node->key == NULL)
-		return (0);
-	new_node->value = strdup(value);
-	if (new_node->value == NULL)
-		return (0);
-	new_node->next = NULL;
 	if (ht->array[index] == NULL)
 	{
+		new_node = create_node(key, value);
+		if (new_node == NULL)
+			return (0);
 		ht->array[index] = new_node;
 	}
 	else
 	{
 		current = ht->array[index];
-		new_node->next = current;
+		while (current != NULL)
+		{
+			if (strcmp(current->key, key) == 0)
+			{
+				current->value = strdup(value);
+				if (current->value == NULL)
+					return (0);
+				return (1);
+			}
+			current = current->next;
+		}
+		new_node = create_node(key, value);
+		if (new_node == NULL)
+			return (0);
+		new_node->next = ht->array[index];
 		ht->array[index] = new_node;
 	}
 	return (1);
